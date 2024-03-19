85ee48c9b83c83759f5c1381bd5bfd6be7c49031
Fabine Specian <fabine.specian@gmail.com>
2024-03-19 14:56:14 +0000

    Implementação a função filtro.

--- src/components/filters/index.js
+++ src/components/filters/index.js
@@ -7,22 +7,22 @@ export const renderFilter = () => {
   devs.innerHTML = `<div class="comandos flex-center">
   <label for="filtro">Filtrar por:</label>
   <select id="filtro" data-testid="select-filter" name="tipo">
-    <option value="todos|todos">Formação Sugerida</option>
-    <option value="formacaoSugerida|Administração">Administração</option>
-    <option value="formacaoSugerida|Ciência da Computação">Ciência da Computação</option>
-    <option value="formacaoSugerida|Design de Interface">Design de Interface</option>
-    <option value="formacaoSugerida|Design de Jogos">Design de Jogos</option>
-    <option value="formacaoSugerida|Engenharia Elétrica">Engenharia Elétrica</option>
-    <option value="formacaoSugerida|Engenharia de Computação">Engenharia de Computação</option>
-    <option value="formacaoSugerida|Engenharia de Produção">Engenharia de Produção</option>
-    <option value="formacaoSugerida|Engenharia de Redes">Engenharia de Redes</option>
-    <option value="formacaoSugerida|Engenharia de Software">Engenharia de Software</option>
-    <option value="formacaoSugerida|Estatística">Estatística</option>
-    <option value="formacaoSugerida|Marketing">Marketing</option>
-    <option value="formacaoSugerida|Matemática">Matemática</option>
-    <option value="formacaoSugerida|Redes de Computadores">Redes de Computadores</option>
-    <option value="formacaoSugerida|Segurança da Informação">Segurança da Informação</option>
-    <option value="formacaoSugerida|Sistemas de Informação">Sistemas de Informação</option>
+    <option value="todos|todos">Formação</option>
+    <option value="formacao|Administração">Administração</option>
+    <option value="formacao|Ciência da Computação">Ciência da Computação</option>
+    <option value="formacao|Design de Interface">Design de Interface</option>
+    <option value="formacao|Design de Jogos">Design de Jogos</option>
+    <option value="formacao|Engenharia Elétrica">Engenharia Elétrica</option>
+    <option value="formacao|Engenharia de Computação">Engenharia de Computação</option>
+    <option value="formacao|Engenharia de Produção">Engenharia de Produção</option>
+    <option value="formacao|Engenharia de Redes">Engenharia de Redes</option>
+    <option value="formacao|Engenharia de Software">Engenharia de Software</option>
+    <option value="formacao|Estatística">Estatística</option>
+    <option value="formacao|Marketing">Marketing</option>
+    <option value="formacao|Matemática">Matemática</option>
+    <option value="formacao|Redes de Computadores">Redes de Computadores</option>
+    <option value="formacao|Segurança da Informação">Segurança da Informação</option>
+    <option value="formacao|Sistemas de Informação">Sistemas de Informação</option>
   </select>
   
   <label for="ordenacao">Ordenar por: </label>
--- src/views/homePage/index.js
+++ src/views/homePage/index.js
@@ -1,28 +1,106 @@
 //import { renderFooter } from './components/footer/index.js';
 //import { renderHeader } from './components/header/index.js';
-
+//import { renderCards } from '../../components/cards/index.js';
 import { renderFilter } from '../../components/filters/index.js';
 import { data } from '../../data/dataset.js';
 import { filterData, sortData, computeStats } from '../../lib/dataFunctions.js';
-
-/*export function homePage(props) {
-  console.log("HOME");
-    const viewEl = document.createElement('div');
-      viewEl.innerHTML = `
-      <h1>DataVerse</h1>
-    `;
-    return viewEl;
-  }*/
-
-
+//import { renderItems, renderStatsMessage } from '../../components/cards/index.js';
 
 const homePage = () => {
+    let cardData = [...data];
+
     const homeElement = document.createElement('div');
-    //homeElement.innerHTML = data;
     homeElement.appendChild(renderFilter());
+
+    const filtro = homeElement.querySelector("#filtro");
+
+    filtro.addEventListener("change", (e) => {
+        console.log(e.target.value);
+
+        let value = e.target.value;
+        const filterBy = value.split("|")[0];
+        value = value.split("|")[1];
+
+        if (value === "todos") {
+            cardData = [...data];
+        } else {
+            cardData = filterData(data, filterBy, value);
+        }
+
+        // cardsItens.innerHTML = "";
+        // cardsItens.appendChild(renderCards(cardData));
+        //cardsItens.innerHTML = "";
+
+        if (value !== "todos") {
+            //const count = computeStats(data, filterBy, value);
+            //const statsMessage = renderStatsMessage(count);
+            // cardsItens.appendChild(statsMessage);
+        }
+
+        //cardsItens.appendChild(renderItems(cardData));
+        console.log(cardData);
+    });
+
+    //homeElement.appendChild(renderCards());
     return homeElement;
 }
 
+
+/*const cardsItens = document.querySelector("#root");
+const filtro = document.querySelector("#filtro");
+
+let cardData = [...data];
+
+filtro.addEventListener("change", (e) => {
+  let value = e.target.value;
+  const filterBy = value.split("|")[0];
+  value = value.split("|")[1];
+  if (value === "todos") {
+    cardData = [...data];
+  } else {
+    cardData = filterData(data, filterBy, value);
+  }
+  cardsItens.innerHTML = "";
+
+  cardsItens.appendChild(renderItems(cardData));
+
+  cardsItens.innerHTML = "";
+
+  if (value !== "todos") {
+    const count = computeStats(data, filterBy, value);
+    const statsMessage = renderStatsMessage(count);
+    cardsItens.appendChild(statsMessage);
+  }
+
+  cardsItens.appendChild(renderItems(cardData));
+});
+
+const ordenacao = document.querySelector("#ordenacao");
+
+ordenacao.addEventListener("change", (e) => {
+  const orderValue = e.target.value;
+
+  if (orderValue === "asc") {
+    cardData = sortData(cardData, "mediaSalarialJr", "asc");
+  }
+  else if (orderValue === "desc") {
+    cardData = sortData(cardData, "mediaSalarialJr", "desc");
+  }
+
+  cardsItens.innerHTML = "";
+  cardsItens.appendChild(renderItems(cardData));
+});
+
+cardsItens.appendChild(renderItems(data));
+
+const botaoLimpar = document.querySelector("#reset-button");
+botaoLimpar.addEventListener("click", () => {
+  cardData = [...data];
+  cardsItens.innerHTML = "";
+  filtro.value = "";
+  ordenacao.value = "";
+  cardsItens.appendChild(renderItems(data));
+})*/
 
 
 export { homePage };
