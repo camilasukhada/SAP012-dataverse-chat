a0738806bcfa1f9f2f5ae95eb2216c9c5a28a8e9
camilasukhada <camilasukhada@gmail.com>
2024-03-26 17:35:39 -0300

    botão iniciar chat

--- src/components/buttons/index.js
+++
@@ -1,6 +0,0 @@
-/*export const renderButtons = () => {
-
-    const buttons = document.createElement('h1');
-    buttons.textContent = "botão";
-    return buttons
-  }*/
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,16 @@
 export const renderHeader = () => {
+  // Criação do elemento h1
   const header = document.createElement('h1');
-  header.textContent = "Mulheres na Tech";
-  return header
+  header.textContent = "Mulheres Tech";
+
+  // Criação do elemento img
+  const image = document.createElement('img');
+  image.src = "assets/icones/mulherestech.png";
+  image.alt = 'ícone mulheres tech'; 
+
+  // Anexa a imagem ao cabeçalho
+  header.appendChild(image);
+
+  // Retorna o cabeçalho completo
+  return header;
 }
