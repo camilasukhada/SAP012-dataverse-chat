20648377cf98c94b64427d98ed85d70fb3ebcce6
Fabine Specian <fabine.specian@gmail.com>
2024-04-03 22:13:01 +0100

    ativação da openAI

--- src/components/chatBox/index.js
+++ src/components/chatBox/index.js
@@ -4,22 +4,89 @@ export const renderChatBox = () => {
 
   const chatBox = document.createElement('div');
   chatBox.classList.add('chat-box');
-  chatBox.innerHTML = `<p id="status"></p>
-    <div id="history">
-      <!-- Aqui vai o chat gerado conforme as respostas -->
-    </div>
-    <div class="text">
-    <input type="text" id="message-input" placeholder="Pergunte aqui...">
-    <button class="btn-submit" id="btn-submit">Enviar</button>
-  </div>`;
+  chatBox.innerHTML = `<div class="container">
+  <div class="persona-list">
+         <ul id="bookList"></ul>
+  </div>
+  <div class="persona-chat">
+      <div id="conversation"> 
+      
+      </div>
+      <div class="caixa_de_texto">
+          <input type="text" id="message" placeholder="Pergunte aqui..">
+          <button id="sendMessageBtn">Enviar</button>
+      </div>
+  </div>
+</div>`;
 
-
-  const btnEnviar = chatBox.querySelector(".btn-submit");
+  const btnEnviar = chatBox.querySelector("#sendMessageBtn");
   btnEnviar.addEventListener("click", () => {
-    communicateWithOpenAI();
-    
 
+    communicateWithOpenAI("Administrador de Banco de Dados", "Oláááá");
+    //colocar o parâmetro 
    });
+
+   const startConversation = async (id) => {
+    idPersonaName = id; 
+
+   const sendMessage = async () => {
+    const messageInput = el.querySelector('#message'); 
+    const message = messageInput.value.trim();
+    if (message) {
+        const conversationArea = el.querySelector('#conversation');
+        const userMessage = document.createElement('p');
+        userMessage.classList.add('user-answer'); 
+        userMessage.textContent = `${message}`;
+        conversationArea.appendChild(userMessage);
+        messageInput.value = '';
+
+        try {
+            const response = await communicateWithOpenAI(bookName, message); // 
+            console.log("Resposta da API do OpenAI:", response); // Log da resposta completa da API
+            if (response && response.choices && response.choices.length > 0) {
+                const botMessageText = response.choices[0].message.content;
+                if (botMessageText) {
+                    const botMessage = document.createElement('p');
+                    botMessage.textContent = `${botMessageText.trim()}`;
+                    botMessage.classList.add('persona-answer'); 
+                    conversationArea.appendChild(botMessage);
+                    conversationArea.scrollTop = conversationArea.scrollHeight; // Rolando para o final da conversa
+                    console.log("Mensagem do bot adicionada à área de conversa:", botMessage);
+                } else {
+                    console.error("A mensagem do bot está vazia ou indefinida.");
+                }
+            } else {
+                console.error("Resposta da API do OpenAI em um formato inesperado:", response);
+            }
+        } catch (error) {
+            console.error("Erro ao comunicar com a API do OpenAI:", error);
+        }
+    }
+};
+
+const messageInput = el.querySelector('#message');
+const sendMessageBtn = el.querySelector('#sendMessageBtn');
+
+// Adicione o event listener para a tecla Enter
+messageInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        sendMessage();
+    }
+});
+
+// Desabilitar o campo de entrada de texto e o botão de envio de mensagens inicialmente
+messageInput.disabled = true;
+sendMessageBtn.disabled = true;
+
+// Adicionar evento de clique no botão de envio de mensagem
+sendMessageBtn.addEventListener('click', sendMessage);
+
+rootElements.insertAdjacentElement("beforebegin", header());
+rootElements.insertAdjacentElement("afterend", footer());
+rootElements.insertAdjacentElement("afterend", el); 
+
+return el;
+};
 
   return chatBox
 };
--- src/lib/openAIApi.js
+++ src/lib/openAIApi.js
@@ -1,10 +1,8 @@
 import { getApiKey } from '../lib/apiKey.js';
 
-let idPersonaName = '';
-
 const apiKey = getApiKey();
 
-export const communicateWithOpenAI = async (idName, userMessage) => {
+export const communicateWithOpenAI = async (name, userMessage) => {
     const resposta = await fetch("https://api.openai.com/v1/chat/completions", {
         method: "POST",
         body: JSON.stringify({
@@ -12,11 +10,12 @@ export const communicateWithOpenAI = async (idName, userMessage) => {
             messages: [
                 {
                     role: "system",
-                    content: `Agora você é a profissional de tecnologia ${idPersonaName}. Deve responder conforme a profissão de ${idPersonaName}, levando em consideração para a personalidade de ${idPersonaName}. Seja objetivo nas respostas`
+                    content: `Agora você é a profissional de tecnologia ${name}. Deve responder conforme a profissão de ${name}, levando em consideração para a personalidade de ${name}. Seja objetivo nas respostas`
                 },
                 {
                     role: "user",
                     content: userMessage
+                    //Precisamos guardar o histórico da conversa, pra ela não "se apresentar" a cada pergunta.
                 }
             ]
         }),
@@ -30,156 +29,3 @@ export const communicateWithOpenAI = async (idName, userMessage) => {
     console.log("Resposta da API do OpenAI:", respostaJson);
     return respostaJson;
 };
-
-
-
-
-
-
-
-
-/*export const communicateWithOpenAI = async (messages) => {
-  const url = 'https://api.openai.com/v1/chat/completions';
-  const apiKey = getApiKey(); // Supondo que getApiKey seja uma função válida que retorna a chave de API
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`
-      },
-      body: JSON.stringify({
-        model: "gpt-3.5-turbo",
-        messages
-      })
-    });
-
-    if (!response.ok) {
-      throw new Error('Failed to fetch');
-    }
-    const data = await response.json();
-
-    if (data.choices && data.choices.length > 0 && data.choices[0].message) {
-      return {
-        success: true,
-        content: data.choices[0].message.content,
-      };
-    } else {
-      throw new Error('Nenhuma mensagem encontrada');
-    }
-  } catch (error) {
-    throw new Error(error);
-  }
-};*/
-
-
-
-
-/*Aqui você pode usar a função communicateWithOpenAI com as mensagens fornecidas nos modelos da documentação
-const messages = [
-  {
-    role: "system",
-    content: "You are a helpful assistant."
-  },
-  {
-    role: "user",
-    content: "Hello!"
-  }
-];
-communicateWithOpenAI(messages)
-  .then(response => {
-    console.log("Resposta do modelo:", response.content);
-  })
-  .catch(error => {
-    console.error("Erro ao se comunicar com a API da OpenAI:", error);
-  });*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*export const communicateWithOpenAI = (messages) => {
-   console.log(messages);
-   /*const url = 'https://api.openai.com/v1/chat/completions';
-
-   const apiKey = getApiKey();
-
-   const requestOptions = {
-      method: 'POST',
-      headers: {
-        'Accept': "application/json",
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`
-      },
-      body: JSON.stringify({
-         model:
-
-      })
-};*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*export const communicateWithOpenAI = (messages) => {
-   const url = 'https://api.openai.com/v1/chat/completions';
-   const api_key = getApiKey();
-};*/
-
-
-
-/*export function sendMessage() {
-   const inputMessage = document.getElementById("message-input").value;
-   alert(inputMessage);*/
-
-
-
-   /*message.addEventListener("click", () => {
-      {
-      return;
-   }*/
